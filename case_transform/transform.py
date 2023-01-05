d8b96fd2de639b0d75a2eb6bfa590797d5b8c556
Ivan Iliin <iliin.ivig@gmail.com>
2023-01-05 21:03:52 +0300

    transform: raise exception

--- case_transform/transform.py
+++ case_transform/transform.py
@@ -10,6 +10,6 @@ def transform(
     from_style = CaseStyle.detect(identifier) if from_style is None else from_style
 
     if from_style is None:
-        ValueError(f'Identifier "{identifier}" doesnt match any of {CaseStyle}.')
+        raise ValueError(f'Identifier "{identifier}" doesnt match any of {CaseStyle}.')
 
     return to_style(*from_style.split(identifier))
